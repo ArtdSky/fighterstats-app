f4f7cd1e014a3755e68a6a4bd903fed2fdfa1be4
Artem <adpm@list.ru>
2024-11-04 22:14:42 +0600

    заменил breeze на sanctum

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,15 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
+
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout']);
+
 
 Route::get('/', function () {
     return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+})->name('dashboard');
 
-require __DIR__.'/auth.php';
+
+
